d6a2d2075077df8347fd8b6ffae8feb6505df4b1
wl-hsu <wlhsu.github@gmail.com>
2023-05-31 14:52:38 -0400

    shopping list init

--- proj/src/app/shopping-list/shopping-list.component.ts
+++ proj/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredients } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredients[] = [
+    new Ingredients('Apples', 5),
+    new Ingredients('Tomatoes', 10),
+  ];
 }
